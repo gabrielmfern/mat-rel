ca71226e55b2450561dd30f92b9c09382bf30851
Gabriel Miranda <gabrielmfern@outlook.com>
2021-11-15 15:00:09 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'mrl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     if (environment.production) {
-      this.router.events.subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          // console.log(event.urlAfterRedirects);
-          (window as any).ga('set', 'page', event.urlAfterRedirects);
-          (window as any).ga('send', 'pageview');
-        }
-      });
+      this.setUpAnalytics();
     }
+  }
+
+  setUpAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-V7TEH9BV37', {
+          page_path: event.urlAfterRedirects
+        });
+      });
   }
 }
