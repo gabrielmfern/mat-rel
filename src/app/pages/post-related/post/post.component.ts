75ac2eb3d2bb0427f58b36477796c9518586c7b3
Gabriel Miranda <gabrielmfern@outlook.com>
2021-12-14 23:20:42 -0300

    fixed problem with liking and disliking posts

--- src/app/pages/post-related/post/post.component.ts
+++ src/app/pages/post-related/post/post.component.ts
@@ -91,7 +91,7 @@ export class PostComponent implements OnInit {
   }
 
   async agree() {
-    if (!this.hasAgreed && !this.isAuthor && this.authService.isLoggedIn && !this.loading) {
+    if (!this.hasAgreed() && !this.isAuthor() && this.authService.isLoggedIn && !this.loading) {
       const disagreedIndex = this.post.disagreed.map((u) => u._id).indexOf(this.loggedUser._id);
       if (disagreedIndex > -1) {
         this.post.disagreed.splice(disagreedIndex);
@@ -108,7 +108,7 @@ export class PostComponent implements OnInit {
   }
 
   async disagree() {
-    if (!this.hasDisagreed && !this.isAuthor && this.authService.isLoggedIn && !this.loading) {
+    if (!this.hasDisagreed() && !this.isAuthor() && this.authService.isLoggedIn && !this.loading) {
       const agreedIndex = this.post.agreed.map((u) => u._id).indexOf(this.loggedUser._id);
       if (agreedIndex > -1) {
         this.post.agreed.splice(agreedIndex);
